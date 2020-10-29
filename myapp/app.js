ff5eda7be568221d928172d827c63965a2732b65
jakubzbrowski98 <jzebrowski@edu.cdv.pl>
2020-10-29 12:10:40 +0100

    njj

--- myapp/app.js
+++ myapp/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const helmet = require("helmet");
 const app = express()
-const port = 3000
-
+const port = 3000 
+ 
+app.use(helmet());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
