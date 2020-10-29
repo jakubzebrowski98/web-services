a5521b9ecb040a72062f2d1964104b31a66ac154
jakubzbrowski98 <jzebrowski@edu.cdv.pl>
2020-10-29 12:19:57 +0100

    start

---
+++ myapp/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const helmet = require("helmet");
+const app = express()
+const port = 3000 
+var bodyParser = require('body-parser')
+
+
+app.use(helmet());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.post('/soap', function (req, res) {
+    res.send('Got a POST request. This is Soap')
+})
+app.post('/rest', function (req, res) {
+    res.send('Got a POST request. This is rest')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
